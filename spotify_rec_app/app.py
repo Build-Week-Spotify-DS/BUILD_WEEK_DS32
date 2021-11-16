46c242e745ae05075c058435477a44435fd8a288
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2021-11-16 00:32:27 -0500

    clean up

--- spotify_rec_app/app.py
+++ spotify_rec_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from predict import pickle_model
-# from flask_sqlalchemy import SQLAlchemy
+
 
 
 APP = Flask(__name__)
@@ -10,7 +10,7 @@ APP = Flask(__name__)
 def index():
     """Base view."""
     return render_template('index.html', title="Spotify Song Suggester")
-    # return f"HOME PAGE"
+   
 
 
 @ APP.route('/submit', methods=['GET'])
@@ -23,17 +23,5 @@ def submit(message=None):
 
     return render_template('index.html', result=result)
 
-
-# APP.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-# DB = SQLAlchemy(APP)
-# class Record(DB.Model):
-#     id = DB.Column(DB.Integer, primary_key=True)
-#     datetime = DB.Column(DB.String(25))
-#     # datetime = DB.Column(DB.DateTime)
-#     value = DB.Column(DB.Float, nullable=False)
-#     def __repr__(self):
-#         return f"[id: {self.id},\
-#                 datetime: {self.datetime},\
-#                 value: {self.value}]"
 if __name__ == "__main__":
     APP.run(debug=True)
