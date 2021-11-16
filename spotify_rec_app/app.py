65570ba7daaffca19f26f0e90ab00a23e9e4df13
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2021-11-16 14:05:05 -0500

    errorfix2

--- spotify_rec_app/app.py
+++ spotify_rec_app/app.py
@@ -2,16 +2,15 @@ from flask import Flask, render_template, request
 from .predict import pickle_model
 
 
-
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
+    @app.route("/", methods=['GET', 'POST'])
     def index():
         """Base view."""
         return render_template('index.html', title="Spotify Song Suggester")
 
-    @ app.route('/submit', methods=['GET'])
+    @ app.route('/submit', methods=['GET', 'POST'])
     def submit(message=None):
         '''run array throu a pickle model'''
 
@@ -22,4 +21,3 @@ def create_app():
         return render_template('index.html', result=result)
 
     return app
-
